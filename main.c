b00895894020739b55ea6e2e23f0b7dfa00aa9e0
kevinari26 <60223120+kevinari26@users.noreply.github.com>
2020-04-16 22:55:43 +0700

    Update main.c
    menyatukan menu.c dengan fungsi-fungsi dari mainalgorithm.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 /* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020 
 * MODUL 8 – TUGAS BESAR
 * Kelompok : B1
-* Hari dan Tanggal : Selasa, 4 April 2020
+* Hari dan Tanggal : Kamis, 16 April 2020
 * Asisten (NIM) : Akmal Narendra Sakti (13213034)
 * Nama File : main.c
 * Deskripsi : Algoritma utama 
@@ -14,6 +14,7 @@
 
 int main()
 {
+    /*
     int row_in; 
     int col_in; 
     int n;
@@ -36,7 +37,76 @@ int main()
     displayGen(grid,row_in,col_in);
     scanf("%d",&n);
     }
+    */
 
+    /* VARIABLE LOCAL */
+    char pilihan;
+    char reinput; // (bool (Y/N))
+    int row_in; 
+    int col_in; 
+    int n, i;
+    char namafile[50];
+            // Asumsi maks. namafile sepanjang 50 karakter
+            // tanpa spasi
 
+    /* ALGORITMA */
+    // Menghapus console
+    do
+    {
+        printf("================== GAME OF LIFE ==================\n\n");
+        printf("Sebuah permainan simulasi pola\n");
+        printf("Game ini dimainkan oleh 0 pemain, hanya bergantung\n");
+        printf("pada input pola awal. Program akan meminta file \n");
+        printf("eksternal berisi pola yang disebut seed.\n\n");
+        printf("==================================================\n");
+        // Input file pola
+        printf("Masukan nama file :");
+        scanf("%s", namafile);
+
+        // Procedure cetak Seed
+        int **grid=newSeed(&row_in,&col_in,namafile);
+        displayGen(grid,row_in,col_in);
+        
+        // Menu pilihan
+        do
+        {
+            printf("== Menu pilihan ==\n");
+            printf(" 1. Tick    (mencetak next pattern 1 kali)\n");
+            printf(" 2. Animate (mencetak next pattern N kali)\n");
+            printf(" 3. Quit    (ganti seed / keluar)\n");
+            printf("Masukan angka pilihan :");
+            scanf(" %c", &pilihan);
+
+            if(pilihan == '1')
+            {
+                nextGen(grid,row_in,col_in);
+                displayGen(grid,row_in,col_in);
+            } else if (pilihan == '2')
+            {
+                // -> Procedure animate
+                printf("Masukkan banyak n iterasi :");
+                scanf("%d", &n);
+                for (i=0;i<n;i++) {
+                    nextGen(grid,row_in,col_in);
+                    displayGen(grid,row_in,col_in);
+                }
+            } else if (pilihan == '3')
+            {
+
+            } else
+            {
+                printf("Input salah !\n");
+            }
+        } while (pilihan != '3');
+
+        // Menu quit
+        printf("Apakah anda akan memasukan file seed baru (Y/N)? ");
+        scanf(" %c", &reinput);
+
+    }while(reinput == 'Y');
+
+    printf("Menutup program...\n");
+    printf("Terima kasih !\n");
+    
     return 0;
 }
